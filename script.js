23e80ffd625ce8979464290e0c496e6cb483bcc3
jaovitin498 <joaovitorscoparoschettino@gmail.com>
2026-04-01 13:58:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// formulario
+
+const name = document.getElementById("nome").value
+const number =  document.getElementById("telefone").value
+const msg = document.getElementById("msg").value
+
+const labarra = 5532988024802
+
+
+function enviar(){
+    const text = "olá, me chamo ${name}. ${number}"
+
+    const textCodificado = encodeURIComponent(text)
+
+    const url = "https://wa.me/${labarra}?text=${textCodificado}";
+
+    window.open(url, "_blank")
+}
+
+
+//scroll suave
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("show")
+        }else{
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const elements = document.querySelectorAll(".hidden")
+
+elements.forEach( (element) => myObserver.observe(element))
